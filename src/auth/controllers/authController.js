d24edd4b0cb8619372bd035034e74ce04c6b8441
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-10-18 12:45:56 +0300

    added signup fields checking

--- src/auth/controllers/authController.js
+++ src/auth/controllers/authController.js
@@ -1,14 +1,52 @@
 'use strict';
 
-const { signup, getUserById, getUserByEmail, getUserByMobile, getUserIdFromToken ,createProfile} = require('../models/user')
-const { createToken,deleteToken } = require('../models/jwt')
+const { signup, getUserById, getUserByEmail, getUserByMobile, getUserIdFromToken, createProfile } = require('../models/user')
+const { createToken, deleteToken } = require('../models/jwt')
+const { validateEmail, validatePassword, checkPassword } = require('./helpers');
+
 
 const signupHandler = async (req, res, next) => {
     try {
+        let { email, password, country_code, mobile, country, city, first_name, last_name } = req.body;
+
+        if (!email || !password || !country_code || !mobile || !country || !city || !first_name || !last_name) {
+            const error = new Error('Missing parameters, please fill all the required fields!');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        if (!validateEmail(email)) {
+            const error = new Error('The email is not valid');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        if (!validatePassword(password)) {
+            const error = new Error('Invalid password format, password should at least have 1 Capital letter, 1 small letter, 1 special character and a number, ex:Ax@123');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        let emailCheck = await getUserByEmail(email);
+
+        if(emailCheck){
+            const error = new Error('This email is already in use, please write a different email address!');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        let mobileCheck = await getUserByMobile(mobile);
+
+        if(mobileCheck){
+            const error = new Error('This mobile is already in use, please write a different mobile number!');
+            error.statusCode = 403;
+            throw error;
+        }
+
         let result = await signup(req.body)
         await createProfile(result);
         let userTokens = await createToken(result.id)
-        res.status(200).json({ accessToken: userTokens.access_token, refreshToken: userTokens.refresh_token, result })
+        res.status(200).json({ accessToken: userTokens.access_token, refreshToken: userTokens.refresh_token})
     } catch (error) {
         res.send(error.message)
     }
@@ -42,4 +80,4 @@ const signOutHandler = async (req, res, next) => {
 
 
 
-module.exports = { signupHandler, signInHandler, signOutHandler}
+module.exports = { signupHandler, signInHandler, signOutHandler }
