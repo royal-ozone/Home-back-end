7a8720319c2b112812bfd64b6704d13dad86b945
Emran Muhammad Aloul <emranaloul1992@hotmail.com>
2022-01-08 23:34:26 +0200

    connect managment_api

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -3,7 +3,8 @@ const { getTokenRecord } = require('../models/jwt');
 const { authenticateWithToken } = require('../models/helpers');
 const { getProfileByUserId, getStoreIdByProfileId, getCompanyByProfileId,getCourierByProfileId } = require('../models/user')
 const {getCartByProfileId} = require('../../api/models/cart')
-
+const axios = require('axios')
+// require('dotenv').config()
 module.exports = async (req, res, next) => {
     try {
         if (!req.headers.authorization) {
@@ -11,15 +12,18 @@ module.exports = async (req, res, next) => {
         }
         let token = req.headers.authorization.split(' ').pop();
 
-        let tokenRecord = await getTokenRecord(token);
+        let tokenRecord = await getTokenRecord(token) ;
+        console.log("🚀 ~ file: bearer.js ~ line 18 ~ module.exports= ~ `${process.env.MANAGEMENT_API}/employee`", `${process.env.MANAGEMENT_API}/employee`)
+        let result = await axios({
+            method: 'GET',
+            url: `${process.env.MANAGEMENT_API}/employee`,
+            headers: {authorization:req.headers.authorization}
+        })
+        console.log("🚀 ~ file: bearer.js ~ line 22 ~ module.exports= ~ result", result)
+        // console.log("🚀 ~ file: bearer.js ~ line 20 ~ module.exports= ~ result", result)
         
-        if (!tokenRecord) {
-            res.status(403).json({
-                status: 403,
-                message: 'Invalid token!',
-            });
-        }
-        else {
+        if (tokenRecord) {
+
             let validUser = await authenticateWithToken(token, 'access');
             let userProfile = await getProfileByUserId(validUser.id);
             let store = await getStoreIdByProfileId(userProfile.id);
@@ -34,10 +38,20 @@ module.exports = async (req, res, next) => {
             req.user.cart_id = cart? cart.id : null;
             req.user.store_id = store ? store.id : null;
             req.user.courier_company_id = company? company.id : null;
-            req.user.courier_id = courier ? courier.id : null;
-            
+            req.user.courier_id = courier ? courier.id : null; 
             
             next();
+        } 
+        else if (result.data.id){
+            req.employee = result.data
+            next();
+        }
+        else {
+           
+            res.status(403).json({
+                status: 403,
+                message: 'Invalid token!',
+            });
         }
 
     } catch (error) {
