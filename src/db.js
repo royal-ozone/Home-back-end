25ba937770eae3b4f406117c880a0ffc4f689fd6
Emran Muhammad Aloul <emranaloul1992@hotmail.com>
2022-01-13 00:47:14 +0200

    adding it

--- src/db.js
+++ src/db.js
@@ -1,7 +1,11 @@
 
 const {Client} = require('pg');
-const client = new Client({connectionString:process.env.DATABASE_URL}) ;
-
+// const client = new Client({connectionString:process.env.DATABASE_URL}) ;
+const client = new Client( {
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.LOCALLY ? false : {rejectUnauthorized: false}
+  } );
+  
 
 module.exports =client;
 
