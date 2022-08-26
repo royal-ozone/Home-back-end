f54d8131ed47a83a118a5ea75612ec8bc0813ebd
emranaloul <emranaloul1992@hotmail.com>
2022-08-26 23:44:33 +0300

    finance work

--- src/api/controllers/orderControllers.js
+++ src/api/controllers/orderControllers.js
@@ -354,7 +354,15 @@ const addTransactions = async (s1, s2, s3) => {
   })
 }
 
-setTimeout(() => { addTransactions('delivered', 'accepted', 'released'); addTransactions('delivered', 'canceled', 'canceled'); addTransactions('canceled', 'canceled', 'canceled'); addTransactions('canceled', 'canceled', 'pending'); addTransactions('delivered', 'canceled', 'pending'); addTransactions('delivered', 'accepted', 'pending') }, 1000)
+setTimeout(() => {
+  //  addTransactions('delivered', 'accepted', 'released'); 
+addTransactions('delivered', 'canceled', 'canceled'); 
+addTransactions('canceled', 'canceled', 'canceled'); 
+addTransactions('canceled', 'canceled', 'pending'); 
+addTransactions('delivered', 'canceled', 'pending');
+//  addTransactions('delivered', 'accepted', 'pending') 
+}
+ , 1000)
 setInterval(() => automatedUpdateOrder({ from: 'accepted', to: 'ready to be shipped' }), 5000)
 setInterval(() => automatedUpdateOrder({ from: 'ready to be shipped', to: 'delivered' }), 10000)
 module.exports = {
